434c307eafa3245bbe2421e711932f3fbf920af4
lockpin010 <116346668+0xCD4@users.noreply.github.com>
2024-01-06 14:08:39 +0300

    Update Average.asm

--- Assembly/Code/Average.asm
+++ Assembly/Code/Average.asm
@@ -1,38 +1,40 @@
 .MODEL SMALL
 .STACK 100H
 
-
 .DATA
-    ARRAY1 DB 3,6,6,1
-    SUM DB 0  
-    AVERAGE DB ?
-    
-    
+ARRAY1 DB  3, 3, 6, 8, 5, 5
+SUM DB 0
+AVERAGE DB ?
+
 .CODE
-    MAIN PROC
-        MOV AX, @DATA
-        MOV DS, AX
-        
-        MOV SI, OFFSET ARRAY1
-        MOV CX, 4
-        
-        LOOP1:
-        MOV DL, [SI]
-        ADD SUM, DL
-        INC SI
-        
-        LOOP LOOP1
-        MOV AL, SUM
-        MOV AH, 0
-        
-        MOV BL, 4
-        DIV BL
-        
-        MOV AVERAGE, AL
-        
-        
-       MOV AH, 4CH 
-       INT 21H
-       
-       MAIN ENDP
-    END MAIN
+MAIN PROC
+MOV AX, @DATA
+MOV DS, AX
+
+MOV SI, OFFSET ARRAY1
+MOV CX, 
+
+PUSH CX   
+
+LOOP1:
+MOV DL, [SI]
+ADD SUM, DL
+INC SI
+LOOP LOOP1
+
+POP CX    ;
+
+MOV AL, SUM
+MOV AH, 0
+
+MOV BL, CL  
+
+DIV BL
+
+MOV AVERAGE, AL
+
+MOV AH, 4CH
+INT 21H
+
+MAIN ENDP
+END MAIN
